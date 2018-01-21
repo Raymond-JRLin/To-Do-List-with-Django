662d70d69e5d644881886cf7a0198423aa8a34d7
Junrui Line <raymond.jr.lin@gmail.com>
2018-01-20 22:42:12 -0500

    created database model

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True) # detailed description
+    completed = models.BooleanField(default=False) # flag of status, default is incompleted, i.e. False
+    created_at = models.DateTimeField(auto_now_add=True) # the timestamp when created
+    updated_at = models.DateTimeField(auto_now=True) # every timestamp when we update
+
+    class Meta:
+        ordering = ('completed', '-updated_at',) # order data as if they are completed, then order them as updating time
