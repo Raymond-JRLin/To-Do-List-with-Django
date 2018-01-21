4ad3a84c2baa6b324cb32d1b7316fe9b09ce3d5c
Junrui Line <raymond.jr.lin@gmail.com>
2018-01-20 22:52:44 -0500

    set urls for admin, main page, delete and complete

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
+from django.contrib import admin
 from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^admin/', admin.site.urls), # administration
+    # url(r'^$', views.index, name='index'),
+    url(r'^$', views.todolist), # main page
+    url(r'^delete/(?P<pk>\d+)/', views.delete), # delete a todo-item
+    url(r'^complete/(?P<pk>\d+)/', views.complete), # complete a todo-item
 ]
