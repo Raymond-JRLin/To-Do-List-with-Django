3515beb2d9cf1cbb7ba15e175c11c14e505d9731
Junrui Line <raymond.jr.lin@gmail.com>
2018-01-21 01:57:31 -0500

    added a class to test

--- todolist/views.py
+++ todolist/views.py
@@ -1,8 +1,30 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-from django.http import HttpResponse
-import datetime
 
-def index(request):
-    message = 'current time is: {}'.format(datetime.datetime.now())
-    return HttpResponse(message)
+class Todo:
+    def __init__(self, description, isCompleted):
+        self.description = description
+        self.isCompleted = isCompleted
+
+
+def todo_list(request):
+    list = []
+    
+    for value in range(0, 10):
+        list.append(Todo("item" + str(value), False))
+
+    return render(request, 'todolist.html', locals())
+
+
+
+
+
+
+###
+# from django.http import HttpResponse
+# import datetime
+#
+# def index(request):
+#     message = 'current time is: {}'.format(datetime.datetime.now())
+#     return HttpResponse(message)
+###
