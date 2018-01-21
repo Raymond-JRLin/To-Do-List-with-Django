95c2d8c6a480ee2df57ede98b55ca3357a8d0c29
Junrui Line <raymond.jr.lin@gmail.com>
2018-01-20 20:17:36 -0500

    corrected some typos

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', get_time)
+    url(r'^$', get_time),
     url(r'', include('todolist.urls'))
 ]
